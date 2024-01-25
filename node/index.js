6dcf3678b67bb660fb4107f8745cdbeca1842090
shinvk <victor.kamiguchi@gmail.com>
2024-01-25 00:14:09 -0300

    ajusta docker compose

--- node/index.js
+++ node/index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const app = express();
 const port = 3000;
+
 const config = {
   host: 'db',
-  user: 'root',
-  password: 'root',
-  database: 'nodedb',
-};
+  user: 'admin',
+  password: 'admin',
+  database: 'nodedb'
+}
 
 app.get('/', async (req, res) => {
   const connection = await mysql.createConnection(config);
